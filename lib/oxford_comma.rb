a7e4ccb1a3ce0fbb56065e9ed4d508dde0ae597e
Dean L Betty <deanlloyd64@gmail.com>
2019-09-10 21:42:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
+#def oxford_comma(array)
+#  if array.length == 1
+#    array.join
+#  elsif array.length == 2
+#    array.join(" and ")
+#  else
+#    z = ", and " + array.pop
+#    array.join(", ") + z
+#  end
+#end
+
 def oxford_comma(array)
   if array.length == 1
     array.join
